d9ad353102c855e8bc556ea65705000c193c5731
aizatibraimova <97326728+aizatibraimova@users.noreply.github.com>
2023-05-13 21:33:27 -0500

    Bookshelf component

---
+++ my-bookshelf/src/Bookshelf.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import './App.css';
+
+const Bookshelf = () => {
+    const [books, setBooks] = useState([]);
+
+    const addBook = () => {
+        const bookName = document.getElementById("book-name").value;
+        const bookWidth = getRandomNumber(30, 70);
+        const bookHeight = getRandomNumber(120, 160);
+        const bookColor = getRandomColor();
+        const newBook = {
+          name: bookName,
+          width: bookWidth,
+          height: bookHeight,
+          color: bookColor,
+        };
+        setBooks((prevBooks) => [...prevBooks, newBook]);
+      };
+    
+      const getRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      };
+    
+      const getRandomColor = () => {
+        const colors = ["red", "blue", "green", "yellow", "purple"];
+        return colors[Math.floor(Math.random() * colors.length)];
+      };
+
+      return (
+        <div className="App">
+          <div className="bookshelf">
+            <div className="shelf"></div>
+            {books.map((book, i) => (
+              <div
+                className={`book ${book.color}`}
+                key={i}
+                style={{ width: `${book.width}px`, height: `${book.height}px` }}
+              >
+                {book.name}
+              </div>
+            ))}
+        </div>
+
+        <form>
+            <label htmlFor="book-name">Book Name:</label>
+            <input type="text" id="book-name" name="book-name" />
+            <button type="button" onClick={addBook}>Add Book</button>
+        </form>
+        </div>
+    )
+}
+
+export default Bookshelf;
