2a6f159d39ab4b40d5ce4c11cc8bfe39a1fa924b
aizatibraimova <97326728+aizatibraimova@users.noreply.github.com>
2023-05-22 21:54:29 -0500

    Header component

--- my-bookshelf/src/App.js
+++ my-bookshelf/src/App.js
@@ -1,10 +1,23 @@
-
-import Bookshelf from './Bookshelf';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Bookshelf from "./components/Bookshelf";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Bookshelf/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="bookshelf" element={<Bookshelf />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+
+      <Bookshelf />
     </div>
   );
 }
---
+++ my-bookshelf/src/components/LoginModal.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// const LoginModal = ({ onClose }) => {
+//   return (
+//     <div className="modal">
+//       <div className="modal-content">
+//         <h2>Login</h2>
+        
+//         <button onClick={onClose}>Close</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LoginModal;
